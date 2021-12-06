519561c62c71bf7bcd4317fc14245a6229f5e177
sonhv82 <sonhv82@yahoo.com>
2021-12-06 13:03:51 +0700

    Update twilio.service.ts

--- server/src/twilio/twilio.service.ts
+++ server/src/twilio/twilio.service.ts
@@ -39,9 +39,9 @@ export class TwilioService {
 	
 	var url = "https://ci-demo-voice-notification.s3.ap-southeast-1.amazonaws.com/data/ddab2dfdfda614f84db7.mp3";
 	if (message.toLowerCase() == "script1"){
-		url = "https://ci-demo-voice-notification.s3.ap-southeast-1.amazonaws.com/data/scrip1.mp3";
+		url = "https://ci-demo-voice-notification.s3.ap-southeast-1.amazonaws.com/data/script1.mp3";
 	}else if (message.toLowerCase() == "script1"){
-		url = "https://ci-demo-voice-notification.s3.ap-southeast-1.amazonaws.com/data/scrip2.mp3";
+		url = "https://ci-demo-voice-notification.s3.ap-southeast-1.amazonaws.com/data/script2.mp3";
 	}else{
 		
 	}
